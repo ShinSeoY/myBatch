cea0a640889963c9dd67f29dbed17967bc5d3f18
ShinSeoY <tjdudtls312@gmail.com>
2024-03-26 21:36:21 +0900

    - notification repository

---
+++ src/main/java/com/my/batch/service/NotificationService.java
@@ -0,0 +1,15 @@
+package com.my.batch.service;
+
+import com.my.batch.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+    private final NotificationRepository notificationRepository;
+
+
+}
